99906a7a9fa1d52b5083cb558755b9a477a75d05
Will Hansen <will.r.s.hansen@gmail.com>
2022-10-12 08:46:19 -0700

    Fix screen to world

--- src/input.rs
+++ src/input.rs
@@ -5,14 +5,14 @@ use euclid::*;
 use termion::event::{Event, Key, MouseButton, MouseEvent};
 
 pub struct InputMap {
-    prev_mouse_pos: Point2D<i32>,
+    prev_mouse_square: Square,
     terminal_size: (u16, u16), // (width, height)
 }
 
 impl InputMap {
     pub fn new(width: u16, height: u16) -> InputMap {
         InputMap {
-            prev_mouse_pos: point2((width / 2) as i32, (height / 2) as i32),
+            prev_mouse_square: point2((width / 2) as i32, (height / 2) as i32),
             terminal_size: (width, height),
         }
     }
@@ -52,18 +52,16 @@ impl InputMap {
             },
             Event::Mouse(me) => match me {
                 MouseEvent::Press(MouseButton::Left, term_x, term_y) => {
-                    let (x, y) = self.screen_to_world(&(term_x, term_y));
-                    let square = Square::new(x, y);
+                    let square = self.screen_to_world(&(term_x, term_y));
                     game.set_player_position(&square).unwrap_or_default();
                 }
                 MouseEvent::Press(MouseButton::Right, term_x, term_y) => {
-                    let (x, y) = self.screen_to_world(&(term_x, term_y));
-                    self.prev_mouse_pos = point2(x, y);
+                    self.prev_mouse_square = self.screen_to_world(&(term_x, term_y));
                 }
                 MouseEvent::Hold(term_x, term_y) => {
-                    let (x, y) = self.screen_to_world(&(term_x, term_y));
+                    let square = self.screen_to_world(&(term_x, term_y));
                     //game.place_line_of_blocks(self.prev_mouse_pos, (x, y), game.selected_block);
-                    self.prev_mouse_pos = point2(x, y);
+                    self.prev_mouse_square = square;
                 }
                 _ => {}
             },
@@ -74,7 +72,7 @@ impl InputMap {
     fn screen_to_world(&self, terminal_position: &(u16, u16)) -> Square {
         // terminal indexes from 1, and the y axis goes top to bottom
         Square::new(
-            terminal_position.0 as i32 - 1,
+            (terminal_position.0 as i32 - 1) / 2,
             self.terminal_size.1 as i32 - terminal_position.1 as i32,
         )
     }
@@ -96,10 +94,37 @@ mod tests {
 
     #[test]
     fn test_screen_to_world__upper_left() {
+        // 50 squares x 50 squares
         let input_map = InputMap::new(100, 50);
         let terminal_pos: (u16, u16) = (1, 1);
         let world_pos = input_map.screen_to_world(&terminal_pos);
         let correct_world_pos = Square::new(0, 49);
         assert_eq!(correct_world_pos, world_pos);
+    }
+    #[test]
+    fn test_screen_to_world__lower_right() {
+        let input_map = InputMap::new(100, 50);
+        let terminal_pos: (u16, u16) = (100, 50);
+        let world_pos = input_map.screen_to_world(&terminal_pos);
+        let correct_world_pos = Square::new(49, 0);
+        assert_eq!(correct_world_pos, world_pos);
+    }
+    #[test]
+    fn test_screen_to_world__upper_right() {
+        let input_map = InputMap::new(100, 50);
+        let terminal_pos: (u16, u16) = (100, 1);
+        let world_pos = input_map.screen_to_world(&terminal_pos);
+        let correct_world_pos = Square::new(49, 49);
+        assert_eq!(correct_world_pos, world_pos);
+    }
+    #[test]
+    fn test_screen_to_world__two_characters_one_square() {
+        let input_map = InputMap::new(100, 50);
+        // odd to even, moving right, is same square
+        let terminal_pos1: (u16, u16) = (35, 25);
+        let terminal_pos2: (u16, u16) = (36, 25);
+        let world_pos1 = input_map.screen_to_world(&terminal_pos1);
+        let world_pos2 = input_map.screen_to_world(&terminal_pos2);
+        assert_eq!(world_pos1, world_pos2);
     }
 }
