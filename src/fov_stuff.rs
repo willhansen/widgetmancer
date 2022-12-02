85a9cf388012c04f9e397a99d68f7e3d2565af23
Will Hansen <will.r.s.hansen@gmail.com>
2022-12-02 08:24:22 -0800

    more view angle stuff.   WIP

--- src/fov_stuff.rs
+++ src/fov_stuff.rs
@@ -1,9 +1,13 @@
+use std::collections::{HashMap, HashSet};
+
+use euclid::{vec2, Angle};
+use ordered_float::OrderedFloat;
+
 use crate::glyph::glyph_constants::SPACE;
 use crate::utility::{
-    octant_to_outward_and_across_directions, SquareSet, WorldPoint, WorldSquare,
-    WorldSquareGlyphMap,
+    octant_to_outward_and_across_directions, SquareSet, WorldMove, WorldPoint, WorldSquare,
+    WorldSquareGlyphMap, STEP_DOWN_LEFT, STEP_DOWN_RIGHT, STEP_UP_LEFT, STEP_UP_RIGHT,
 };
-use std::collections::{HashMap, HashSet};
 
 pub struct Line {
     p1: WorldPoint,
@@ -26,6 +30,11 @@ impl FovResult {
     }
 }
 
+pub struct ClosedClockWiseAngleInterval {
+    start_angle: Angle<f32>,
+    end_angle: Angle<f32>,
+}
+
 pub fn field_of_view_from_square(
     start_square: WorldSquare,
     sight_blockers: SquareSet,
@@ -37,8 +46,54 @@ pub fn field_of_view_from_square(
         for outward_steps in 1..sight_radius {
             for across_steps in 0..=outward_steps {
                 let square = start_square + outward_dir * outward_steps + across_dir * across_steps;
+                let square_angle_interval = angle_interval_of_square(start_square, square);
             }
         }
     }
     todo!()
+}
+
+pub fn angle_interval_of_square(
+    sight_center: WorldSquare,
+    blocking_square: WorldSquare,
+) -> ClosedClockWiseAngleInterval {
+    assert_ne!(sight_center, blocking_square);
+    let relative_square = blocking_square - sight_center;
+    let rel_square_center = relative_square.to_f32();
+    let rel_square_corners: Vec<WorldMove> = vec![
+        rel_square_center + STEP_UP_RIGHT.to_f32() * 0.5,
+        rel_square_center + STEP_UP_LEFT.to_f32() * 0.5,
+        rel_square_center + STEP_DOWN_LEFT.to_f32() * 0.5,
+        rel_square_center + STEP_DOWN_RIGHT.to_f32() * 0.5,
+    ];
+
+    let center_angle = rel_square_center.angle_from_x_axis();
+    let corner_angles: Vec<Angle<f32>> = rel_square_corners
+        .iter()
+        .map(|rel_corner_point| rel_corner_point.angle_from_x_axis())
+        .collect();
+
+    let most_clockwise = corner_angles
+        .iter()
+        .min_by_key(|&&c| OrderedFloat(center_angle.angle_to(c).radians))
+        .unwrap();
+    let least_clockwise = corner_angles
+        .iter()
+        .max_by_key(|&&c| OrderedFloat(center_angle.angle_to(c).radians))
+        .unwrap();
+
+    ClosedClockWiseAngleInterval {
+        start_angle: *least_clockwise,
+        end_angle: *most_clockwise,
+    }
+}
+#[cfg(test)]
+mod tests {
+    use ntest::assert_false;
+    use pretty_assertions::{assert_eq, assert_ne};
+    use rgb::RGB8;
+
+    use super::*;
+    #[test]
+    fn test_square_view_angle() {}
 }
