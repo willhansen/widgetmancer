3cf192d5111367a5cbbddc8b5d5dc2c12dedb76a
Will Hansen <will.r.s.hansen@gmail.com>
2023-11-20 18:34:15 -0800

    add flake.lock

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Widgetmancer dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    #stable.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+			      cargo
+			      rustc
+			      cargo-watch
+			      rustfmt
+			      rust-analyzer
+			      clippy
+			      lldb
+			      gdb
+					];
+          shellHook = "zsh";
+        };
+      });
+}
+
