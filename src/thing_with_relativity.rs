8305ec98f05a5ee710c3260be62202d47d678dff
Will Hansen <will.r.s.hansen@gmail.com>
2024-02-27 14:48:37 -0800

    Minor cleanup

--- src/thing_with_relativity.rs
+++ src/thing_with_relativity.rs
@@ -1,11 +1,12 @@
 use paste::paste;
 use std::marker::PhantomData;
+use std::ops::{Add, Sub};
 use typenum::*;
 
-#[derive(Hash, Copy, Clone, Eq)]
-pub struct ThingWithRelativity<THING_TYPE, RELATIVITY_LEVEL = typenum::U0>
+#[derive(Hash, Clone, Eq)]
+pub struct ThingWithRelativity<THING_TYPE, RELATIVITY_LEVEL>
 where
-    RELATIVITY_LEVEL: typenum::Unsigned,
+    RELATIVITY_LEVEL: Unsigned,
 {
     pub thing: THING_TYPE,
     pub _level_of_relativity: std::marker::PhantomData<RELATIVITY_LEVEL>,
@@ -13,7 +14,7 @@ where
 
 impl<THING_TYPE, RELATIVITY_LEVEL> ThingWithRelativity<THING_TYPE, RELATIVITY_LEVEL>
 where
-    RELATIVITY_LEVEL: typenum::Unsigned,
+    RELATIVITY_LEVEL: Unsigned,
 {
     pub fn new_thing(new_thing: THING_TYPE) -> Self {
         ThingWithRelativity {
@@ -31,8 +32,14 @@ impl<T, RELATIVITY_LEVEL> HasRelativity for ThingWithRelativity<T, RELATIVITY_LE
     type RelativityLevel = RELATIVITY_LEVEL;
 }
 
-use std::ops::{Add, Sub};
-macro_rules! op_with_relativity {
+impl<T, R> Copy for ThingWithRelativity<T, R>
+where
+    T: Copy,
+    R: Unsigned,
+{
+}
+
+macro_rules! make_op_with_relativity {
     ($trait:ident, $func:ident, $rel_trait:ident, $rel_func:ident) => {
         trait $rel_trait<T, REL_DIFF>
         where
@@ -41,6 +48,7 @@ macro_rules! op_with_relativity {
             type Output;
             fn $rel_func(self, rhs: T) -> Self::Output;
         }
+
         impl<T, L_REL, R_REL, DIFF_REL> $trait<ThingWithRelativity<T, R_REL>>
             for ThingWithRelativity<T, L_REL>
         where
@@ -57,40 +65,29 @@ macro_rules! op_with_relativity {
         }
     };
 }
-op_with_relativity!(Add, add, AddWithRelativity, add_with_relativity);
-op_with_relativity!(Sub, sub, SubWithRelativity, sub_with_relativity);
-
-impl<T, L_REL, R_REL> AddWithRelativity<ThingWithRelativity<T, R_REL>, U1>
-    for ThingWithRelativity<T, L_REL>
-{
-    type Output = Self;
-    fn add_with_relativity(self, rhs: ThingWithRelativity<T, R_REL>) -> Self::Output {
-        self.thing.add(rhs.thing())
-    }
+macro_rules! impl_op_with_relativity {
+    ($trait:ident, $func:ident, $rel_trait:ident, $rel_func:ident, $rhs_rel_diff:ty, $out_rel_diff:ty) => {
+        impl<T, L_REL, R_REL> $rel_trait<ThingWithRelativity<T, R_REL>, $rhs_rel_diff>
+            for ThingWithRelativity<T, L_REL>
+        {
+            type Output = ThingWithRelativity<T, Sum<L_REL, $out_rel_diff>>;
+            fn $rel_func(self, rhs: ThingWithRelativity<T, R_REL>) -> Self::Output {
+                self.thing.$func(rhs.thing())
+            }
+        }
+    };
 }
-impl<T, L_REL, R_REL> SubWithRelativity<ThingWithRelativity<T, R_REL>, U1>
-    for ThingWithRelativity<T, L_REL>
-{
-    type Output = Self;
-    fn sub_with_relativity(self, rhs: ThingWithRelativity<T, R_REL>) -> Self::Output {
-        self.thing.sub(rhs.thing())
-    }
-}
-
-impl<T, L_REL, R_REL> SubWithRelativity<ThingWithRelativity<T, R_REL>, U0>
-    for ThingWithRelativity<T, L_REL>
-{
-    type Output = ThingWithRelativity<T, Add1<L_REL>>;
-    fn sub_with_relativity(self, rhs: ThingWithRelativity<T, R_REL>) -> Self::Output {
-        self.thing.sub(rhs.thing())
-    }
-}
+make_op_with_relativity!(Add, add, AddWithRelativity, add_with_relativity);
+make_op_with_relativity!(Sub, sub, SubWithRelativity, sub_with_relativity);
+impl_op_with_relativity!(Add, add, AddWithRelativity, add_with_relativity, U1, U0);
+impl_op_with_relativity!(Sub, sub, SubWithRelativity, sub_with_relativity, U1, U0);
+impl_op_with_relativity!(Sub, sub, SubWithRelativity, sub_with_relativity, U0, U1);
 
 impl<THING_TYPE, RELATIVITY_LEVEL> std::fmt::Debug
     for ThingWithRelativity<THING_TYPE, RELATIVITY_LEVEL>
 where
     THING_TYPE: std::fmt::Debug,
-    RELATIVITY_LEVEL: typenum::Unsigned,
+    RELATIVITY_LEVEL: Unsigned,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
@@ -106,7 +103,7 @@ impl<THING_TYPE, RELATIVITY_LEVEL> euclid::num::Zero
     for ThingWithRelativity<THING_TYPE, RELATIVITY_LEVEL>
 where
     THING_TYPE: euclid::num::Zero,
-    RELATIVITY_LEVEL: typenum::Unsigned,
+    RELATIVITY_LEVEL: Unsigned,
 {
     fn zero() -> Self {
         ThingWithRelativity::new_thing(THING_TYPE::zero())
@@ -117,7 +114,7 @@ impl<THING_TYPE, RELATIVITY_LEVEL> std::ops::Neg
     for ThingWithRelativity<THING_TYPE, RELATIVITY_LEVEL>
 where
     THING_TYPE: std::ops::Neg,
-    RELATIVITY_LEVEL: typenum::Unsigned,
+    RELATIVITY_LEVEL: Unsigned,
 {
     type Output = ThingWithRelativity<<THING_TYPE as std::ops::Neg>::Output, RELATIVITY_LEVEL>;
 
@@ -130,16 +127,13 @@ impl<THING_TYPE, RHS_TYPE, RELATIVITY_LEVEL> std::ops::Mul<RHS_TYPE>
     for ThingWithRelativity<THING_TYPE, RELATIVITY_LEVEL>
 where
     THING_TYPE: std::ops::Mul<RHS_TYPE>,
-    RELATIVITY_LEVEL: typenum::Unsigned,
+    RELATIVITY_LEVEL: Unsigned,
 {
     type Output =
         ThingWithRelativity<<THING_TYPE as std::ops::Mul<RHS_TYPE>>::Output, RELATIVITY_LEVEL>;
 
     fn mul(self, rhs: RHS_TYPE) -> Self::Output {
-        ThingWithRelativity {
-            thing: self.thing * rhs,
-            _level_of_relativity: std::marker::PhantomData,
-        }
+        Self::Output::new(self.thing * rhs)
     }
 }
 
@@ -149,7 +143,7 @@ impl<THING_TYPE, RHS_THING_TYPE, RELATIVITY_LEVEL>
     for ThingWithRelativity<THING_TYPE, RELATIVITY_LEVEL>
 where
     THING_TYPE: std::cmp::PartialEq<RHS_THING_TYPE>,
-    RELATIVITY_LEVEL: typenum::Unsigned,
+    RELATIVITY_LEVEL: Unsigned,
 {
     fn eq(&self, other: &ThingWithRelativity<RHS_THING_TYPE, RELATIVITY_LEVEL>) -> bool {
         self.thing == other.thing
@@ -159,12 +153,9 @@ impl<THING_TYPE, T, RELATIVITY_LEVEL> From<(T, T)>
     for ThingWithRelativity<THING_TYPE, RELATIVITY_LEVEL>
 where
     THING_TYPE: From<(T, T)>,
-    RELATIVITY_LEVEL: typenum::Unsigned,
+    RELATIVITY_LEVEL: Unsigned,
 {
     fn from(value: (T, T)) -> Self {
-        ThingWithRelativity {
-            thing: THING_TYPE::from(value),
-            _level_of_relativity: std::marker::PhantomData,
-        }
+        ThingWithRelativity::new(THING_TYPE::from(value))
     }
 }
