802d910a166757008a10c76f57cdc7662f2418ee
Will Hansen <will.r.s.hansen@gmail.com>
2024-08-05 12:34:04 -0700

    Add script for mass search replace of shape operations trait names

---
+++ scripts/update_shape_trait_names.bash
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+set -o pipefail -o errexit -o nounset -o xtrace
+
+cd src/utility/geometry
+
+rg "\b\w+Ops" -o --no-filename | uniq | tee /tmp/original_names.txt |sed -E "s/([A-Z][a-z]*)/\l\1_/g"|sed -E "s/_ops_$/::Operations/" | tee /tmp/new_names.txt > /dev/null
+
+paste -d" " /tmp/original_names.txt /tmp/new_names.txt | xargs -n2 bash -c 'echo $0 a $1 b' 
