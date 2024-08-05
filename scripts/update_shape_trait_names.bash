7189a81ccb9b239396fe56637cac7cc4a82aa202
Will Hansen <will.r.s.hansen@gmail.com>
2024-08-05 12:41:11 -0700

    get search replace to working state

--- scripts/update_shape_trait_names.bash
+++ scripts/update_shape_trait_names.bash
@@ -6,4 +6,4 @@ cd src/utility/geometry
 
 rg "\b\w+Ops" -o --no-filename | uniq | tee /tmp/original_names.txt |sed -E "s/([A-Z][a-z]*)/\l\1_/g"|sed -E "s/_ops_$/::Operations/" | tee /tmp/new_names.txt > /dev/null
 
-paste -d" " /tmp/original_names.txt /tmp/new_names.txt | xargs -n2 bash -c 'echo $0 a $1 b' 
+paste -d" " /tmp/original_names.txt /tmp/new_names.txt | xargs -n2 bash -c 'sed -Ei "s/$0/$1/g" $(find . -type f -name "*")' 
