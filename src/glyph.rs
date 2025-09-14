eaea00f6fc7961c2ed6461a60c3037a5015f2b30
Will Hansen <will.r.s.hansen@gmail.com>
2025-09-13 17:50:56 -0700

    tests and geometry

--- src/glyph.rs
+++ src/glyph.rs
@@ -10,6 +10,7 @@ use termion::color;
 
 use glyph_constants::*;
 use utility::geometry2::IPointExt;
+use utility::geometry2::PointExt;
 
 use crate::floating_square::character_for_half_square_with_1d_offset;
 use crate::screen::{
